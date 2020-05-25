d796ccceb8be1aa6022dbbe38dca8492ef71f490
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-25 18:02:31 +0900

    뷰페이저용 어댑터 클래스 생성

---
+++ app/src/main/java/kr/co/tjoeun/fragmentandviewpager_20200525/adapters/MyViewPagerAdapter.java
@@ -0,0 +1,37 @@
+package kr.co.tjoeun.fragmentandviewpager_20200525.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import kr.co.tjoeun.fragmentandviewpager_20200525.fragments.FirstFragment;
+import kr.co.tjoeun.fragmentandviewpager_20200525.fragments.SecondFragment;
+import kr.co.tjoeun.fragmentandviewpager_20200525.fragments.ThirdFragment;
+
+public class MyViewPagerAdapter extends FragmentPagerAdapter {
+
+    public MyViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        if(position == 0 ) {
+            return new FirstFragment();
+        }
+        else if(position == 1) {
+            return new SecondFragment();
+        }
+        else {
+            return new ThirdFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
